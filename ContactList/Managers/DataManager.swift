751d32d4ff0b7a45548345cec24c693f2ad02d1a
Nikolay Trofimov <nikktro@gmail.com>
2022-02-18 23:58:02 +0300

    add random user data

--- ContactList/Managers/DataManager.swift
+++ ContactList/Managers/DataManager.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class DataManager {
+    let names: [String] = ["Judith", "Carlos", "Russell", "Louis", "Christine", "Rebecca", "David", "Benjamin", "Raymond", "Edward"]
+    let surnames: [String] = ["Bailey", "Young", "Jenkins", "Nelson", "Bell", "Smith", "Clark", "Martinez", "Henderson", "Anderson"]
+    let phoneNumbers: [String] = ["555-8157", "555-7147", "555-5768", "555-6816", "555-2539", "555-6065", "555-6196", "555-3344", "555-2160", "555-8814"]
+}
