a33dca5e0fdf6aec454b888fdf436655bd00917b
Nikolay Trofimov <nikktro@gmail.com>
2022-02-19 01:42:10 +0300

    ContactListTableViewController and ContactSectionListTableViewController logic

--- ContactList/ViewControllers/ContactListTableViewController.swift
+++ ContactList/ViewControllers/ContactListTableViewController.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class ContactListTableViewController: UITableViewController {
-
+    
+    private let contactList = MainTabBarController.contactList
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactList", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        let contact = contactList[indexPath.row]
+        
+        content.text = contact.fullname
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 }
 
--- ContactList/ViewControllers/ContactSectionListTableViewController.swift
+++ ContactList/ViewControllers/ContactSectionListTableViewController.swift
@@ -9,4 +9,36 @@ import UIKit
 
 class ContactSectionListTableViewController: UITableViewController {
 
+    private let contactList = MainTabBarController.contactList
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        contactList[section].fullname
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactSectionList", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        let contact = contactList[indexPath.section]
+        
+        if indexPath.row == 0 {
+            content.text = contact.phoneNumber
+            content.image = UIImage(systemName: "phone")
+        } else {
+            content.text = contact.email
+            content.image = UIImage(systemName: "tray")
+        }
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 }
